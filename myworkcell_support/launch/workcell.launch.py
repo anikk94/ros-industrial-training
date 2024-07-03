a3dbc5c3eaf1fd48a1dd8d9bdd1143b3d87f4037
Anirudh Krishnan Komaralingam <anikk.umd@gmail.com>
2024-07-03 02:28:47 +0000

    first commit

---
+++ myworkcell_support/launch/workcell.launch.py
@@ -0,0 +1,27 @@
+import launch
+import launch_ros
+
+def generate_launch_description():
+    return launch.LaunchDescription([
+        # launch actions here...
+        launch_ros.actions.Node(
+            name='fake_ar_publisher_node',
+            package='fake_ar_publisher',
+            executable='fake_ar_publisher_node',
+            output='screen',
+        ),
+        launch_ros.actions.Node(
+            name='vision_node',
+            package='myworkcell_core',
+            executable='vision_node',
+            output='screen',
+        ),
+        launch_ros.actions.Node(
+            name='myworkcell_node',
+            package='myworkcell_core',
+            executable='myworkcell_node',
+            output='screen',
+            parameters=[{'base_frame': 'world'},
+                        {'human_frame': 'humanF'}],
+        ),
+    ])
